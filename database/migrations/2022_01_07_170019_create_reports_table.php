bedd2e25f01893d35b5d26d350b72df237b8ddab
hisanori-long <shimakawa.hisanori@gmail.com>
2022-01-08 02:10:55 +0900

    create reports db

---
+++ database/migrations/2022_01_07_170019_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('species_id');
+            $table->string("image_url");
+            $table->geometry('location')->comment('緯度・経度');
+            $table->boolean('sexual')->comment('False(オス) True(メス)');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
