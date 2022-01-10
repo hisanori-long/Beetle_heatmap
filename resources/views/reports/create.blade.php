40a17104390c171db6fdb738cef3e7686df1da32
hisanori-long <shimakawa.hisanori@gmail.com>
2022-01-11 00:42:40 +0900

    create report form

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -32,6 +32,9 @@
 
       lonlat=e.latlng;
       addMarker = L.marker(e.latlng).addTo(map);
+
+      document.getElementById("latlon_id").value = lonlat;
+
     }
   </script>
 </head>
@@ -46,18 +49,23 @@
         <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div id="mapcontainer" style="width:1200px;height:600px"></div>
-                    <form class="mb-6" action="{{ route('reports.store') }}" method="POST">
+                    <form class="mb-6" action="{{ route('reports.store', ['spece_id' =>$spece_id])}}" method="POST" name="myForm" id="myForm">
                         @csrf
-                       <div class="flex flex-col mb-4">
-                            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="tweet">Tweet</label>
-                            <input class="border py-2 px-3 text-grey-darkest" type="text" name="tweet" id="tweet">
-                        </div>
                         <div class="flex flex-col mb-4">
-                            <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="description">Description</label>
-                            <input class="border py-2 px-3 text-grey-darkest" type="text" name="description" id="description">
+                          <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="latlon">発見した場所</label>
+                          <div id="mapcontainer" style="width:1000px;height:500px"></div>
+                          <input id=latlon_id name="latlon" type="text" placeholder="Input">
+
+                          <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="text">大きさ[cm]</label>
+                          <input class="border py-2 px-3 text-grey-darkest" type="text" name="size" id="size">
+
+                          <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" name="sex" for="tweet">性別</label>
+                          {{Form::select('sex', ['オス', 'メス'])}}
+
+                          <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="description">コメント</label>
+                          <input class="border py-2 px-3 text-grey-darkest" type="text" name="comment" id="comment">
                         </div>
-                        <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-black uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+                        <button onclick="funcBtn();" type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-black uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
                             submit
                         </button>
                     </form>
