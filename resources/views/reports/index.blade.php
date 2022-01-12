5802f770e542fef58a2863888ce04f599bbdd3b6
hisanori-long <shimakawa.hisanori@gmail.com>
2022-01-12 14:56:02 +0900

    sent data to index

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -34,6 +34,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                 <div id="mapcontainer" style="width:600px;height:600px"></div>
+                
                 </div>
             </div>
         </div>
